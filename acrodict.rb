161506f695451e493fde3726b6c0022c5eb54d71
Chris Tusa <chris.tusa@leafscale.com>
2020-02-03 21:29:01 -0600

    Formatted comments for Rdoc compatibility.

--- acrodict.rb
+++ acrodict.rb
@@ -1,5 +1,10 @@
+#!/usr/bin/env ruby
+##
+# = AcroDict
+#
 # ----------------------------------------------------
-# AcroDict - based on AcroBot
+# AcroDict :: Acronym Dictionary
+#
 # Chris Tusa <chris.tusa@leafscale.com>
 # v 0.1
 # ----------------------------------------------------
@@ -15,12 +20,19 @@ $LOAD_PATH.unshift('.')
 
 DATAFILE="./tusa.yml"
 
+##
+# Dictfile module handles the I/O to a dictionary database file on the filesystem
+#
+# AcroDict uses yaml/store to reference objects. The contents of the file are
+# retrieved from disk and stored in nemory as a Ruby::Hash object.
+#
 module Dictfile
   require 'yaml/store'
 
+  ##
   # Makes a sample dictionary file and saves it to a file, and returns the hash back to the program.
   # This method requires a single parameter:
-  #   'dictfile' is the output filename to pass to save_data
+  # * 'dictfile' is the output filename to pass to save_data
   #
   def Dictfile::make_datafile(dictfile)
     datahash = { 'new' =>
@@ -37,9 +49,10 @@ module Dictfile
   end
 
 
+  ##
   # Load the YAML Dictionary from file and return it as a Ruby Hash class object
   # This method requires one parameter:
-  #   'dictfile' is the input filename
+  # * 'dictfile' is the input filename
   #
   def Dictfile::load_data(dictfile)
     if File.exist?(dictfile)
@@ -54,10 +67,11 @@ module Dictfile
     return data
   end
 
+  ##
   # Save the Ruby Hash into the YAML Dictionary File
   # This method requires two parameters:
-  #   'dictfile' is the output filename
-  #   'datahash' is the a valid Ruby Hash class object
+  # * 'dictfile' is the output filename
+  # * 'datahash' is the a valid Ruby Hash class object
   #
   def Dictfile::save_data(dictfile, datahash)
     f = File.open(dictfile, "w")
@@ -67,17 +81,23 @@ module Dictfile
 end
 
 
+##
+# Acronym class handles the methods for creating, reading, updating and deleting
+# items from the Dictionary stored in Memory and commiting it back to disk.
 class Acronym
 
+  ##
+  # The dictionary is loaded when calling .new
   def initialize
     @mydata = Dictfile::load_data(DATAFILE)
   end
 
+  ##
   # Adds a value to the Dictionary by specifying the tagname, keyname, and definition.
   # This method requires three parameters:
-  #   'tagname' is the category or first level in the hash
-  #   'keyname' is the acronym key
-  #   'definition'   is the definition
+  # * 'tagname' is the category or first level in the hash
+  # * 'keyname' is the acronym key
+  # * 'definition'   is the definition
   def add(tagname, keyname, definition)
     # See if the acronym already exists
     if @mydata[tagname].has_key?(keyname)
@@ -98,12 +118,14 @@ class Acronym
     return true
   end
 
+
+  ##
   # Delete a value from the Dictionary by specifying the tagname, keyname, and array index.
   # This method requires three parameters:
-  #   'tagname' is the category or first level in the hash
-  #   'keyname' is the acronym key
-  #   'aindex'  is the array index of the specific entry.
-  #             specifying a -1 will delete the entire record.
+  # * 'tagname' is the category or first level in the hash
+  # * 'keyname' is the acronym key
+  # * 'aindex'  is the array index of the specific entry.
+  # *Note* : specifying aindex of -1 will delete the entire record
   #
   # If the array becomes empty, the keyname will be removed from the tag.
   #
@@ -130,33 +152,37 @@ class Acronym
   end
 
 
+  ##
   # Return a list of Tags
   def tags
     return @mydata.keys
   end
 
 
+  ##
   # Return an array of Acronyms by tagname
   # This method requires one parameter:
-  #   'tagname' is the category or first level in the hash
+  # * 'tagname' is the category or first level in the hash
   def list_bytag(tagname)
     return @mydata[tagname].keys
   end
 
+  ##
   # Return an array of definitions by tagname
   # This method requires two parameters:
-  #   'tagname' is the category or first level in the hash
-  #   'keyname' is the acronym key
+  # * 'tagname' is the category or first level in the hash
+  # *  'keyname' is the acronym key
   def getdef(tagname, keyname)
     return @mydata[tagname].fetch(keyname)
   end
 
 
+  ##
   # Find a value in the Dictionary by specifying the tagname and keyname,
   # returns the values as an Array or NIL if not found
   # This method requires two parameters:
-  #   'tagname' is the category or first level in the hash
-  #   'keyname' is the acronym key
+  # * 'tagname' is the category or first level in the hash
+  # * 'keyname' is the acronym key
   #
   def find_bykey(tagname,keyname)
     if @mydata[tagname].has_key?(keyname)
@@ -168,7 +194,7 @@ class Acronym
 end
 
 
-### MAIN PROGRAM EXECUTION FOR TESTING ###
+### MAIN PROGRAM EXECUTION FOR USAGE EXAMPLES ###
 
 # ac = Acronym.new
 # puts ac.find_bykey("new","tbd")
